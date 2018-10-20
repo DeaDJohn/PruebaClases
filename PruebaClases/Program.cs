571b0f4dee666663120b3061e46cc39c180f732e
Juan Jose Fernandez <juanjofb86@gmail.com>
2018-10-20 12:21:13 +0200

    Crear una clase usuario

--- PruebaClases/Program.cs
+++ PruebaClases/Program.cs
@@ -10,6 +10,33 @@ namespace PruebaClases
     {
         static void Main(string[] args)
         {
+            Console.WriteLine();
+            Usuario fermin = new Usuario();
+            fermin.getNombre();
+            fermin.getEmail();
+            Console.ReadKey();
         }
+    }
+
+    class Usuario
+    {
+        public string Nombre;
+        public string Email;
+
+        public Usuario()
+        {
+            Nombre = "Fermin";
+            Email = "fermin@microsoft.com";
+        }
+
+        public void getNombre()
+        {
+            Console.WriteLine( "Mi nombre es {0}", Nombre);
+        }
+        public void getEmail()
+        {
+            Console.WriteLine("Mi email es {0}", Email);
+        }
+
     }
 }
